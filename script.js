c27bbd1c384ed458c2436d46c8918e6474810516
rfdllh291105-crypto <rfdllh291105@gmail.com>
2025-12-19 09:53:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-  let currentSlide = 0;
-  const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
 
-  function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[index].classList.add("active");
-  }
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
 
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-  }
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
 
-  function prevSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    showSlide(currentSlide);
-  }
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
